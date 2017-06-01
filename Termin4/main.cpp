c6c00cb41c0f53471a4b067256187426b8fd34a9
PAddy <Patrick-olinger@web.de>
2017-06-01 13:27:28 +0200

    Tst

--- Termin4/main.cpp
+++ Termin4/main.cpp
@@ -25,7 +25,12 @@ pid_t parentId;
 int8_t memory[185];
 
 int open_memory(mode_t mode){
-    return shm_open(SHM_NAME, O_CREAT, mode);
+    if(getpid() == parentId){
+        if(shm_unlink(SHM_NAME) == 0){
+            cout << "Gelöscht!" << endl;
+        };
+    }
+    return shm_open(SHM_NAME, O_CREAT| mode, NULL);
 }
 
 void close_memory(int filedescriptor){
@@ -63,7 +68,7 @@ int main()
     {
         cout << "CHILD:" << endl;
         descr = open_memory(O_RDONLY);
-        CCommQueue* commQueue = (CCommQueue*)mmap(NULL, getSizeForMMap(), PROT_READ|PROT_WRITE,MAP_SHARED, descr,0);
+        //CCommQueue* commQueue = (CCommQueue*)mmap(NULL, getSizeForMMap(), PROT_READ|PROT_WRITE,MAP_SHARED, descr,0);
         // Child process - Reads all Messages from the Queue and outputs them with auxiliary data
         close_memory(descr);
     }
@@ -72,12 +77,15 @@ int main()
         cout << getSizeForMMap() << endl;
         cout << "PARENT:" << endl;
         descr = open_memory(O_RDWR);
-        CCommQueue* commQueue = (CCommQueue*)mmap(NULL, getSizeForMMap(), PROT_READ|PROT_WRITE,MAP_SHARED, descr,0);
+        if(descr < 0){
+            return descr;
+        }
+        void* commQueue = mmap(0, getSizeForMMap(), PROT_WRITE | PROT_READ,MAP_SHARED, descr,0);
         cout << commQueue << endl;
         cout << errno << endl;
         CBinarySemaphore sem = CBinarySemaphore(true,true);
         new (commQueue)CCommQueue(QUEUE_SIZE,sem);
-        cout << commQueue->getNumOfMessages() << endl;
+        //cout << commQueue->getNumOfMessages() << endl;
         // Parent process - Writes all Messages into the Queue
         close_memory(descr);
 
